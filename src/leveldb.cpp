21f1798ae3451fe32ff2bba2fce22fc5d292e373
kafku <kazunoko93@gmail.com>
2016-06-02 08:56:50 +0900

    add c++ function for opening db

---
+++ src/leveldb.cpp
@@ -0,0 +1,26 @@
+#include <Rcpp.h>
+#include <leveldb/db.h>
+#include <leveldb/write_batch.h>
+
+// [[Rcpp::export]]
+SEXP cppOpenLevelDB(const std::string &path, const bool create)
+{
+  // set options
+  leveldb::Options options;
+  options.create_if_missing = create;
+
+  // open db
+  leveldb::DB *db;
+  leveldb::Status status = leveldb::DB::Open(options, path, &db);
+
+  // check status
+  if(!status.ok()){
+    Rcpp::stop("failed to open db.");
+  }
+
+  Rcpp::XPtr<leveldb::DB> ptr(db);
+
+  return ptr;
+}
+
+
