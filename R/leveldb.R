c516866898afaeeb1f663192bbddb3010cdd000f
kafku <kazunoko93@gmail.com>
2016-06-02 17:45:36 +0900

    add method for putting values

--- R/leveldb.R
+++ R/leveldb.R
@@ -5,6 +5,7 @@ setClass(
   slots = list(path = "character", ptr = "externalptr"))
 
 #' @export
+#' @export
 setGeneric(
   name = "dbGet",
   def = function(db, keys){
@@ -19,6 +20,41 @@ setMethod(
   })
 
 #' @export
+setGeneric(
+  name = "dbPut",
+  def = function(db, keys, values, sync = FALSE){
+    standardGeneric('dbPut')
+  })
+
+put.leveldb <- function(db, keys, values, sync = FALSE){
+  # TODO: check length and dim
+
+  # convert values to string
+  non.character <- !is.character(values)
+  non.character <- !is.character(values)
+  if(any(non.character)){
+    values[non.character] <- as.character(values[non.character])
+  }
+
+  cppDbPut(db@ptr, keys, values, sync)
+}
+
+setMethod(
+  f = "dbPut",
+  signature = signature(db = "LevelDB", keys = "character",
+                        values = "ANY", sync = "logical"),
+  definition = function(db, keys, values, sync){
+    put.leveldb(db, keys, values, sync)
+  })
+
+
+setMethod(
+  f = "dbPut",
+  signature = signature(db = "LevelDB", keys = "character",
+                        values = "ANY", sync = "missing"),
+  definition = function(db, keys, values){
+    put.leveldb(db, keys, values)
+  })
 OpenLevelDB <- function(path, create = FALSE){
   if(!create && !file.exists(path)){
     stop(paste0("cannot find ", path))
--- src/leveldb.cpp
+++ src/leveldb.cpp
@@ -51,3 +51,30 @@ Rcpp::CharacterVector cppDbGet(SEXP xp,
 }
 
 
+
+// [[Rcpp::export]]
+void cppDbPut(SEXP xpt,
+              const std::vector<std::string> &keys,
+              const std::vector<std::string> &values,
+              const bool sync)
+{
+  Rcpp::XPtr<leveldb::DB> ptr(xpt);
+
+  // prepare batch
+  leveldb::WriteBatch batch;
+  for(std::vector<std::string>::const_iterator it_key = keys.begin(), it_val = values.begin();
+      it_key != keys.end(); ++it_key, ++it_val){
+    batch.Put(leveldb::Slice(*it_key),
+              leveldb::Slice(*it_val));
+  }
+
+  // set options
+  leveldb::WriteOptions write_options;
+  write_options.sync = sync;
+
+  // write to db
+  leveldb::Status status = ptr->Write(write_options, &batch);
+
+  // TODO: check status
+}
+
